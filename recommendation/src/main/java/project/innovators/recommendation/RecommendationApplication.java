72a55b7e8cb0355a0e5983ba7abd67fc8b16fd2c
Srijal Joshi <srijal.joshy@gmail.com>
2020-03-10 00:41:29 -0500

    first commit

---
+++ recommendation/src/main/java/project/innovators/recommendation/RecommendationApplication.java
@@ -0,0 +1,14 @@
+package project.innovators.recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class RecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationApplication.class, args);
+	}
+
+}
