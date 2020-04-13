e3d3936fe4f7e291649440418ca79f0b409f1015
Srijal Joshi <srijal.joshy@gmail.com>
2020-04-12 23:01:08 -0500

    Added cart page. TODO: update and delete product

--- recommendation/src/main/java/project/innovators/recommendation/service/ICartItemService.java
+++ recommendation/src/main/java/project/innovators/recommendation/service/ICartItemService.java
@@ -3,9 +3,15 @@ package project.innovators.recommendation.service;
 import project.innovators.recommendation.model.Cart;
 import project.innovators.recommendation.model.CartItem;
 
+import java.util.List;
+
 public interface ICartItemService {
     long addCartItem(CartItem cartItem);
     long removeCartItem(CartItem cartItem);
 
     void save(CartItem cartItem);
+
+    List<CartItem> findByCart(Cart cart);
+
+    void deleteById(Long cartItemId);
 }
